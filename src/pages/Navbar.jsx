6f255f7f9725de26123767e3d1de0f52936b57cb
Herath97747 <ar97747@fhss.sjp.ac.lk>
2023-03-16 16:19:05 +0530

    change text

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                     <div className='col-10 mx-auto'>
 
                         <nav className="navbar navbar-expand-lg navbar-light">
-                            <a className="navbar-brand" href="#">ShahZaib</a>
+                            <a className="navbar-brand" href="#">Deshani...</a>
                             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span className="navbar-toggler-icon"></span>
                             </button>
